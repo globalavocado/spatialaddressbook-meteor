adafe02b61728527f9e8c84b05af529c60167ad4
globalavocado <globalavocado@gmail.com>
2016-05-12 17:35:55 +0100

    markers added and removed through click events

--- server/main.js
+++ server/main.js
@@ -3,3 +3,15 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// marker collection
+var Markers = new Meteor.Collection('markers');
+Meteor.publish("markers", function () {
+  return Markers.find();
+});
+
+// Listen to incoming HTTP requests, can only be used on the server
+WebApp.connectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
+});
